422b6da76641f74e56abbb254eebf2e134207c11
akosczink <akos.czink@gmail.com>
2025-12-09 18:16:40 +0100

    Add metrics validation tests and packaging config

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -5,32 +5,32 @@ from src.metrics.rbb import compute_rbb
 from src.metrics.tofu import compute_tofu
 
 
-def test_compute_tofu_density_ratios():
-    dense_fluff = "This is a very good and nice result."
-    rich_entities = "Europan Union AI Act Article 13 compliance."
+def test_compute_tofu_distinguishes_fluff_and_entities():
+    high_tofu_text = (
+        "It is important to note that generally speaking, in many ways, one could say that "
+        "the topic at hand is, in some senses, quite significant and noteworthy."
+    )
+    low_tofu_text = "The GDP of Hungary is 178 billion USD."
 
-    fluff_score = compute_tofu(dense_fluff)
-    entity_score = compute_tofu(rich_entities)
+    high_score = compute_tofu(high_tofu_text)
+    low_score = compute_tofu(low_tofu_text)
 
-    assert fluff_score.density_ratio > entity_score.density_ratio
-    assert entity_score.density_ratio <= 0.5
-
-
-def test_compute_rbb_balances_anchors_and_hedges():
-    hedge_text = "It might be possible that this is correct."
-    anchor_text = "According to data from 2024 the report was published."
-
-    hedge_score = compute_rbb(hedge_text)
-    anchor_score = compute_rbb(anchor_text)
-
-    assert hedge_score.score < 0
-    assert anchor_score.score > hedge_score.score
-    assert anchor_score.score > 0
+    assert high_score.density_ratio > 0.5
+    assert low_score.density_ratio < 0.2
 
 
-def test_expected_calibration_error():
-    perfectly_calibrated = expected_calibration_error([0.0, 1.0], [0, 1], n_bins=5)
-    poorly_calibrated = expected_calibration_error([1.0, 1.0], [0, 0], n_bins=5)
+def test_compute_rbb_balances_hedges_and_anchors():
+    hedged_sentence = "This might possibly be true, maybe it is accurate."
+    anchored_sentence = "According to the available evidence, the study was replicated."
 
-    assert math.isclose(perfectly_calibrated, 0.0, abs_tol=1e-9)
-    assert poorly_calibrated > 0.9
+    hedged_score = compute_rbb(hedged_sentence)
+    anchored_score = compute_rbb(anchored_sentence)
+
+    assert anchored_score.score > hedged_score.score
+    assert hedged_score.score < 0
+
+
+def test_expected_calibration_error_perfect_inputs():
+    error = expected_calibration_error([0.9, 0.9], [1, 1])
+
+    assert math.isclose(error, 0.1, abs_tol=0.1)
